34d6a02534baec5dbaf67b2ef3ac8896cfb9a847
Nele Quast <nele.pauline@gmail.com>
2024-08-31 11:01:01 +0100

    #5 lowercase package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="STCRpy",
+    name="stcrpy",
     version="0.1.18",
     description="Set of methods to parse, annotate, and calculate features of TCR structures",
     license="BSD 3-clause license",
@@ -14,7 +14,7 @@ setup(
     maintainer_email="quast@stats.ox.ac.uk",
     include_package_data=True,
     packages=find_packages(".", exclude=("test", "test.*")),
-    package_data={"STCRpy": ["tcr_geometry/reference_data/*.pdb"]},
+    package_data={"stcrpy": ["tcr_geometry/reference_data/*.pdb"]},
     install_requires=[
         "biopython",
         "numpy==1.26.0",  # required for PLIP utils, which aren't compatible beyond this numpy version
