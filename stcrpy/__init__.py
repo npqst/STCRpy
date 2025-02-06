77838bba2acf520615182e4ee5bc0e91b8dd7984
Nele Quast <nele.pauline@gmail.com>
2025-02-06 18:45:15 +0000

    #23 fetch data from STCRdab and PDB implemented"

--- stcrpy/__init__.py
+++ stcrpy/__init__.py
@@ -3,4 +3,4 @@ from .tcr_processing.TCRIO import TCRIO
 from .tcr_geometry.TCRAngle import TCRAngle
 from .tcr_geometry.TCRDock import TCRDock
 from .tcr_geometry.TCRGeom import TCRGeom
-from .tcr_methods.tcr_methods import load_TCRs
+from .tcr_methods.tcr_methods import load_TCRs, fetch_TCR
--- stcrpy/tcr_methods/tcr_methods.py
+++ stcrpy/tcr_methods/tcr_methods.py
@@ -1,4 +1,6 @@
 import warnings
+import requests
+import os
 
 from ..tcr_processing.TCRParser import TCRParser
 from .tcr_batch_operations import batch_load_TCRs
@@ -31,3 +33,74 @@ def load_TCRs(tcr_structure_files, tcr_ids=None):
                 f"Length of TCR IDs {len(tcr_ids)} does not match length of files {len(tcr_structure_files)}. TCR IDs reverted to default."
             )
     return batch_load_TCRs(tcr_structure_files)
+
+
+def fetch_TCR(pdb_id: str):
+    """
+    Fetches and parses a T-cell receptor (TCR) structure from the STCRDab or RCSB PDB databases.
+
+    The function first attempts to download a PDB file from the STCRDab database.
+    If the PDB file is not found, it falls back to downloading a CIF file from RCSB PDB.
+    The downloaded file is then parsed using `TCRParser` to extract TCR structures.
+
+    Parameters:
+        pdb_id (str): The PDB identifier of the structure to be fetched.
+
+    Returns:
+        - A single TCR structure if exactly one is found.
+        - A list of TCR structures if multiple are found.
+        - None if no TCRs are identified (with a `UserWarning` issued).
+
+    Raises:
+        - A warning if no TCR structures are found in the downloaded file.
+        - Prints an error message if the file cannot be downloaded.
+
+    Notes:
+        - STCRDab returns an error message if the requested PDB ID does not exist.
+        - The function temporarily saves the downloaded file and deletes it after parsing.
+
+    Example:
+        tcr = fetch_TCR("6eqa")
+
+    """
+
+    stcrdab_base_url = "https://opig.stats.ox.ac.uk/webapps/stcrdab-stcrpred/pdb/"
+    pdb_base_url = "https://files.rcsb.org/download/"
+
+    filename = f"{pdb_id.upper()}.pdb"
+
+    url = stcrdab_base_url + pdb_id.lower()
+    response = requests.get(url, stream=True)
+
+    TCR_FOUND = False
+
+    if response.status_code == 200:
+        with open(filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+            if (
+                not b"does not exist" in chunk
+            ):  # STCRDab returns '$PDB does not exist for downloading' if PDB code not found in database
+                TCR_FOUND = True
+    if not TCR_FOUND:
+        os.remove(filename)  # remove the file written with response from STCRDab
+        filename = f"{pdb_id.upper()}.cif"
+        url = pdb_base_url + filename
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(filename, "wb") as file:
+                for chunk in response.iter_content(chunk_size=1024):
+                    file.write(chunk)
+        else:
+            print("Failed to download file")
+
+    tcr_parser = TCRParser()
+    tcr = list(tcr_parser.get_tcr_structure(pdb_id, filename).get_TCRs())
+    os.remove(filename)
+    if len(tcr) == 1:
+        return tcr[0]
+    elif len(tcr) == 0:
+        warnings.warn(f"No TCRs identified in {pdb_id}")
+        return None
+    else:
+        return tcr
