0ef2adabe2c34ef299eb6c6e50106595f8b9fe40
Nele Quast <nele.pauline@gmail.com>
2025-03-03 14:40:34 +0000

    implemented batch operations for geometry and germline methods, and joint method for full analysis

--- stcrpy/tcr_methods/tcr_batch_operations.py
+++ stcrpy/tcr_methods/tcr_batch_operations.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import warnings
+import os
 
 from ..tcr_processing.TCRParser import TCRParser
 from ..tcr_interactions.TCRInteractionProfiler import TCRInteractionProfiler
 from ..tcr_geometry.TCRGeom import TCRGeom
 from ..tcr_geometry.TCRGeomFiltering import DockingGeometryFilter
+from ..tcr_formats.tcr_formats import get_sequences
 
 
 class TCRBatchOperator:
@@ -63,6 +65,105 @@ class TCRBatchOperator:
 
         return interactions_df
 
+    def get_TCR_geometry(self, tcr_generator, mode="rudolph", save_as_csv=None):
+        geometries_dict = {}
+        for tcr in tcr_generator:
+            if tcr is None:  # handles case where file could not be parsed in generator
+                continue
+            tcr_id = f"{tcr.parent.parent.id}_{tcr.id}"
+            if isinstance(
+                tcr, tuple
+            ):  # handle case where tcr is passed as (key, value)
+                tcr_id, tcr = tcr
+            try:
+                geometries_dict[tcr_id] = tcr.calculate_docking_geometry(
+                    mode=mode, as_df=True
+                )
+            except Exception as e:
+                warnings.warn(
+                    f"Geometry calculation failed for {tcr} with error {str(e)}"
+                )
+        geometries_df = pd.concat(geometries_dict).droplevel(1)
+
+        if save_as_csv is not None:
+            geometries_df.to_csv(save_as_csv)
+
+        return geometries_df
+
+    def get_germlines_and_alleles(self, tcr_generator, save_as_csv=None):
+        germlines_and_alleles_dict = {}
+        for tcr in tcr_generator:
+            if tcr is None:  # handles case where file could not be parsed in generator
+                continue
+            tcr_id = f"{tcr.parent.parent.id}_{tcr.id}"
+            if isinstance(
+                tcr, tuple
+            ):  # handle case where tcr is passed as (key, value)
+                tcr_id, tcr = tcr
+            germlines_and_alleles_dict[tcr_id] = tcr.get_germlines_and_alleles()
+
+        germlines_and_alleles_df = pd.DataFrame(germlines_and_alleles_dict).T
+
+        if save_as_csv is not None:
+            germlines_and_alleles_df.to_csv(save_as_csv)
+
+        return germlines_and_alleles_df
+
+    def full_analysis(self, tcr_generator, geometry_mode="rudolph", save_dir=None):
+        from tqdm import tqdm
+
+        germlines_and_alleles_dict = {}
+        geometries_dict = {}
+        interaction_analysis_dict = {}
+
+        for tcr in tqdm(tcr_generator):
+            if tcr is None:  # handles case where file could not be parsed in generator
+                continue
+            tcr_id = f"{tcr.parent.parent.id}_{tcr.id}"
+            if isinstance(
+                tcr, tuple
+            ):  # handle case where tcr is passed as (key, value)
+                tcr_id, tcr = tcr
+
+            try:
+                germlines_and_alleles_dict[tcr_id] = tcr.get_germlines_and_alleles()
+            except Exception as e:
+                warnings.warn(
+                    f"Germline and allele retrieval failed for {tcr} with error {str(e)}"
+                )
+            try:
+                geometries_dict[tcr_id] = tcr.calculate_docking_geometry(
+                    mode=geometry_mode, as_df=True
+                )
+            except Exception as e:
+                warnings.warn(
+                    f"Geometry calculation failed for {tcr} with error {str(e)}"
+                )
+            try:
+                interaction_analysis_dict[tcr_id] = tcr.profile_peptide_interactions()
+            except Exception as e:
+                warnings.warn(
+                    f"Interaction profiling failed for {tcr} with error {str(e)}"
+                )
+        germlines_and_alleles_df = pd.DataFrame(germlines_and_alleles_dict).T
+
+        geometries_df = pd.concat(geometries_dict).droplevel(1)
+
+        interactions_df = pd.concat(
+            interaction_analysis_dict.values(),
+            keys=interaction_analysis_dict.keys(),
+            axis=0,
+        )
+
+        if save_dir is not None:
+            geometries_df.to_csv(os.path.join(save_dir, "geometries.csv"))
+            germlines_and_alleles_df.to_csv(
+                os.path.join(save_dir, "germlines_and_alleles.csv")
+            )
+            interactions_df.to_csv(os.path.join(save_dir, "interactions.csv"))
+
+        return germlines_and_alleles_df, geometries_df, interactions_df
+
 
 def batch_load_TCRs(tcr_files):
     if isinstance(tcr_files, dict):
@@ -88,3 +189,33 @@ def get_TCR_interactions(tcr_files, renumber=True, save_as_csv=None):
     return batch_ops.get_TCR_pMHC_interactions(
         tcr_generator, renumber=renumber, save_as_csv=save_as_csv
     )
+
+
+def get_TCR_geometry(tcr_files, mode="rudolph", save_as_csv=None):
+    batch_ops = TCRBatchOperator()
+    if isinstance(tcr_files, list):
+        tcr_generator = batch_ops.tcrs_from_file_list(tcr_files)
+    if isinstance(tcr_files, dict):
+        tcr_generator = batch_ops.tcrs_from_file_dict(tcr_files)
+
+    return batch_ops.get_TCR_geometry(tcr_generator, mode=mode, save_as_csv=save_as_csv)
+
+
+def get_germlines_and_alleles(tcr_files, save_as_csv=None):
+    batch_ops = TCRBatchOperator()
+    if isinstance(tcr_files, list):
+        tcr_generator = batch_ops.tcrs_from_file_list(tcr_files)
+    if isinstance(tcr_files, dict):
+        tcr_generator = batch_ops.tcrs_from_file_dict(tcr_files)
+
+    return batch_ops.get_germlines_and_alleles(tcr_generator, save_as_csv=save_as_csv)
+
+
+def analyse_tcrs(tcr_files, save_dir=None):
+    batch_ops = TCRBatchOperator()
+    if isinstance(tcr_files, list):
+        tcr_generator = batch_ops.tcrs_from_file_list(tcr_files)
+    if isinstance(tcr_files, dict):
+        tcr_generator = batch_ops.tcrs_from_file_dict(tcr_files)
+
+    return batch_ops.full_analysis(tcr_generator, save_dir=save_dir)
